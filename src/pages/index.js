09c4b1721d323e450ac11a043d75b8420e629cff
dillionmegida <dillionmegida@gmail.com>
2020-02-17 19:02:51 +0100

    Update config

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Styles from '../styles/index.module.css';
 
-import { Link, graphql } from "gatsby"
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
